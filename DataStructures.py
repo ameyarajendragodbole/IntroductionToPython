a93876d3006fb0752cf4c1fcaa435a94108cfb77
ameyarajendragodbole <dr.agodbole@gmail.com>
2025-02-12 04:14:42 +0000

    Added matplotlib

--- DataStructures.py
+++ DataStructures.py
@@ -1,4 +1,5 @@
 import numpy
+from matplotlib import pyplot as plt
 
 print("Hello World!")
 print("line 2")
@@ -6,3 +7,9 @@ print("line 3")
 
 arr = numpy.array([1, 2, 3, 4, 5])
 print(arr)
+
+x_values = [1, 2, 3, 4]
+y_values = [5, 4, 6, 2]
+
+plt.plot(x_values, y_values)
+plt.show()
